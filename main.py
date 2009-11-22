684709fa81da6f035cc992f6070cbd4ac99be6dd
Carl Chenet <chaica@ohmytux.com>
2009-11-23 00:52:28 +0100

    Added main.py which provides the Main class of Brebis.

---
+++ main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2009 Carl Chenet <chaica@ohmytux.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Brebis main
+"""Brebis main"""
+from cliparse import CliParse
+from configurations import Configurations
+from checkbackups import CheckBackups
+
+class Main(object):
+    """The main class for Brebis"""
+
+    def __init__(self):
+        self.__main()
+
+    def __main(self):
+        _cliparser= CliParse()
+        _options = _cliparser.return_options()
+        print(_options)
+        _confs = Configurations(_options.confpath)
+        CheckBackups(_confs.return_configurations())
+
