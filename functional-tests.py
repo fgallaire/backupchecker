bab9e54382cbf14adde093f96fae31f3c81bd5f4
Carl Chenet <chaica@ohmytux.com>
2011-09-10 23:28:25 +0200

    added functional tests to check the hashes of zip archives

--- functional-tests.py
+++ functional-tests.py
@@ -473,6 +473,114 @@ class Test25_wrong_tar_bz2_archive_sha512_hash:
                 else:
                     print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
 
+class Test26_wrong_zip_archive_md5_hash:
+    def __init__(self):
+        self.__testname = self.__class__.__name__
+        self.__testdir = 'functional-tests/wrong-zip-archive-md5-hash'
+        self.__resultfile = os.path.join(self.__testdir, 'a.out')
+        self.__main()
+
+    def __main(self):
+        retcode = subprocess.call([EXE, OPTCONFIG, self.__testdir, OPTLOG, self.__resultfile])
+        if retcode != 0:
+            print('{} - {}return code:{}'.format(self.__testname, KOMSG, str(retcode)))
+        else:
+            with open(self.__resultfile, 'r') as __file:
+                if '1 file with unexpected hash' in __file.read():
+                    print('{} - {}'.format(self.__testname, OKMSG))
+                else:
+                    print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
+
+class Test27_wrong_zip_archive_sha1_hash:
+    def __init__(self):
+        self.__testname = self.__class__.__name__
+        self.__testdir = 'functional-tests/wrong-zip-archive-sha1-hash'
+        self.__resultfile = os.path.join(self.__testdir, 'a.out')
+        self.__main()
+
+    def __main(self):
+        retcode = subprocess.call([EXE, OPTCONFIG, self.__testdir, OPTLOG, self.__resultfile])
+        if retcode != 0:
+            print('{} - {}return code:{}'.format(self.__testname, KOMSG, str(retcode)))
+        else:
+            with open(self.__resultfile, 'r') as __file:
+                if '1 file with unexpected hash' in __file.read():
+                    print('{} - {}'.format(self.__testname, OKMSG))
+                else:
+                    print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
+
+class Test28_wrong_zip_archive_sha224_hash:
+    def __init__(self):
+        self.__testname = self.__class__.__name__
+        self.__testdir = 'functional-tests/wrong-zip-archive-sha224-hash'
+        self.__resultfile = os.path.join(self.__testdir, 'a.out')
+        self.__main()
+
+    def __main(self):
+        retcode = subprocess.call([EXE, OPTCONFIG, self.__testdir, OPTLOG, self.__resultfile])
+        if retcode != 0:
+            print('{} - {}return code:{}'.format(self.__testname, KOMSG, str(retcode)))
+        else:
+            with open(self.__resultfile, 'r') as __file:
+                if '1 file with unexpected hash' in __file.read():
+                    print('{} - {}'.format(self.__testname, OKMSG))
+                else:
+                    print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
+
+class Test29_wrong_zip_archive_sha256_hash:
+    def __init__(self):
+        self.__testname = self.__class__.__name__
+        self.__testdir = 'functional-tests/wrong-zip-archive-sha256-hash'
+        self.__resultfile = os.path.join(self.__testdir, 'a.out')
+        self.__main()
+
+    def __main(self):
+        retcode = subprocess.call([EXE, OPTCONFIG, self.__testdir, OPTLOG, self.__resultfile])
+        if retcode != 0:
+            print('{} - {}return code:{}'.format(self.__testname, KOMSG, str(retcode)))
+        else:
+            with open(self.__resultfile, 'r') as __file:
+                if '1 file with unexpected hash' in __file.read():
+                    print('{} - {}'.format(self.__testname, OKMSG))
+                else:
+                    print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
+
+class Test30_wrong_zip_archive_sha384_hash:
+    def __init__(self):
+        self.__testname = self.__class__.__name__
+        self.__testdir = 'functional-tests/wrong-zip-archive-sha384-hash'
+        self.__resultfile = os.path.join(self.__testdir, 'a.out')
+        self.__main()
+
+    def __main(self):
+        retcode = subprocess.call([EXE, OPTCONFIG, self.__testdir, OPTLOG, self.__resultfile])
+        if retcode != 0:
+            print('{} - {}return code:{}'.format(self.__testname, KOMSG, str(retcode)))
+        else:
+            with open(self.__resultfile, 'r') as __file:
+                if '1 file with unexpected hash' in __file.read():
+                    print('{} - {}'.format(self.__testname, OKMSG))
+                else:
+                    print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
+
+class Test31_wrong_zip_archive_sha512_hash:
+    def __init__(self):
+        self.__testname = self.__class__.__name__
+        self.__testdir = 'functional-tests/wrong-zip-archive-sha512-hash'
+        self.__resultfile = os.path.join(self.__testdir, 'a.out')
+        self.__main()
+
+    def __main(self):
+        retcode = subprocess.call([EXE, OPTCONFIG, self.__testdir, OPTLOG, self.__resultfile])
+        if retcode != 0:
+            print('{} - {}return code:{}'.format(self.__testname, KOMSG, str(retcode)))
+        else:
+            with open(self.__resultfile, 'r') as __file:
+                if '1 file with unexpected hash' in __file.read():
+                    print('{} - {}'.format(self.__testname, OKMSG))
+                else:
+                    print('{} - {}value in result file not expected'.format(self.__testname, KOMSG))
+
 if __name__ == '__main__':
     Test1_file_missing_in_tar_gz()
     Test2_file_missing_in_tar_bz2()
@@ -499,3 +607,9 @@ if __name__ == '__main__':
     Test23_wrong_tar_bz2_archive_sha256_hash()
     Test24_wrong_tar_bz2_archive_sha384_hash()
     Test25_wrong_tar_bz2_archive_sha512_hash()
+    Test26_wrong_zip_archive_md5_hash()
+    Test27_wrong_zip_archive_sha1_hash()
+    Test28_wrong_zip_archive_sha224_hash()
+    Test29_wrong_zip_archive_sha256_hash()
+    Test30_wrong_zip_archive_sha384_hash()
+    Test31_wrong_zip_archive_sha512_hash()
