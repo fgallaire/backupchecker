c6b4d4f8ba95e6b2fd690deeac6ad8310cacc2df
Carl Chenet <chaica@ohmytux.com>
2013-04-16 00:53:26 +0200

    add wrong xz archive sha512 hash test

--- functionaltests.py
+++ functionaltests.py
@@ -577,6 +577,14 @@ class Test_wrong_gzip_archive_sha512_hash(Main):
         self._resultfile = os.path.join(self._testdir, 'a.out')
         self._main('1 file with unexpected hash')
 
+class Test_wrong_xz_archive_sha512_hash(Main):
+    def __init__(self, q):
+        self._queue = q
+        self._testname = self.__class__.__name__
+        self._testdir = os.path.join(ABSPATH, 'functional-tests/wrong-xz-archive-sha512-hash')
+        self._resultfile = os.path.join(self._testdir, 'a.out')
+        self._main('1 file with unexpected hash')
+
 class Test_wrong_bzip2_archive_md5_hash(Main):
    def __init__(self, q):
        self._queue = q
