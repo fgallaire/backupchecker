89e63989174bba33e90aa989feffbe6a4e3147f7
Carl Chenet <chaica@ohmytux.com>
2013-07-21 14:49:27 +0200

    update to python 3.3 and brebis 0.5

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ import platform
 import sys
 
 # Warn the user about the supported Python versions
-if float(platform.python_version()[0:3]) < 3.2:
-    print('You need at least Python 3.2 to use Brebis')
+if float(platform.python_version()[0:3]) < 3.3:
+    print('You need at least Python 3.3 to use Brebis')
     sys.exit(1)
 
 CLASSIFIERS = [
@@ -28,11 +28,11 @@ CLASSIFIERS = [
     'Environment :: Console',
     'License :: OSI Approved :: GNU General Public License (GPL)',
     'Operating System :: POSIX :: Linux',
-    'Programming Language :: Python :: 3.2'
+    'Programming Language :: Python :: 3.3'
 ]
 
 setup(name = 'brebis',
-    version = '0.4',
+    version = '0.5',
     license = 'GNU GPL v3',
     description = 'automated backup checker',
     long_description = 'Brebis is a fully automated backup checker.',
