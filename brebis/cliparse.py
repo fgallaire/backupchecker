62b2b1eec2dc84855d16720c7170123433b5a56f
Carl Chenet <chaica@ohmytux.com>
2013-08-08 00:48:32 +0200

    bump to good current version in cli help message

--- brebis/cliparse.py
+++ brebis/cliparse.py
@@ -48,7 +48,7 @@ class CliParse:
             metavar='DIR')
         __parser.add_argument('-v', '--version',
             action='version',
-            version='%(prog)s 0.4',
+            version='%(prog)s 0.5',
             help='print the version of this program and exit')
         __parser.add_argument('-l', '--log', dest='logfile',
             action='store',
