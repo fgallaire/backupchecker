62fab6fc103ad92d7859073af2a9e9b1bffe8375
Carl Chenet <chaica@ohmytux.com>
2013-11-06 00:17:22 +0100

    generate the hash sum of the list of files for zip archive and add it in the conf file when -G is invoked

--- brebis/generatelist/generatelistforzip.py
+++ brebis/generatelist/generatelistforzip.py
@@ -76,12 +76,15 @@ class GenerateListForZip(GenerateList):
         self._generate_list(__listconfinfo)
         # call the method to write the configuration file if --gen-full was required
         if self._genfull:
+            # generate the hash sum of the list of files
+            __listhashsum = self._get_list_hash(__listconfinfo['arclistpath'])
             __arcname =  os.path.basename(self.__arcpath[:-4])
             __confinfo = {'arcname': __arcname,
                             'arcpath': self.__arcpath,
                             'arcconfpath': ''.join([self.__arcpath[:-3],'conf']),
                             'arclistpath': __listconfinfo['arclistpath'],
-                            'arctype': 'archive'}
+                            'arctype': 'archive',
+                            'sha512': __listhashsum}
             self._generate_conf(__confinfo)
 
     def __extract_uid_gid(self, __binary):
