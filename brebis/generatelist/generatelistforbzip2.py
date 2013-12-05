60f39c006b09f9d73dbbe72945c6d642d6db0f46
Carl Chenet <chaica@ohmytux.com>
2013-12-05 17:57:37 +0100

    support to print configuration file and file list to different directories or to swith default output directory for bzip2 archives

--- brebis/generatelist/generatelistforbzip2.py
+++ brebis/generatelist/generatelistforbzip2.py
@@ -32,6 +32,9 @@ class GenerateListForBzip2(GenerateList):
         __arcpath = __genparams['arcpath']
         __delimiter = __genparams['delimiter']
         self._genfull = __genparams['genfull']
+        self.__listoutput = __genparams['listoutput']
+        self.__confoutput = __genparams['confoutput']
+        self.__fulloutput = __genparams['fulloutput']
         __listoffiles = ['[files]\n']
         __filetype = 'f'
         __filehash = get_hash(bz2.BZ2File(__arcpath, 'r'), 'md5')
@@ -40,8 +43,18 @@ class GenerateListForBzip2(GenerateList):
                                 os.path.split(__arcpath)[-1][:-4],
                                 __filetype,
                                 __filehash))
+        # define the flexible file list path
+        __arcwithext = os.path.split(''.join([__arcpath[:-3], 'list']))[1]
+        if self.__listoutput:
+            __arclistpath = os.path.join(self.__listoutput, __arcwithext)
+        elif self.__fulloutput:
+            __arclistpath = os.path.join(self.__fulloutput, __arcwithext)
+        else:
+            # default
+            __arclistpath = ''.join([__arcpath[:-3], 'list'])
+            
         # call the method to write information in a file
-        __listconfinfo = {'arclistpath': ''.join([__arcpath[:-3], 'list']),
+        __listconfinfo = {'arclistpath': __arclistpath,
                             'listoffiles':  __listoffiles}
         self.__lci = __listconfinfo
         self._generate_list(__listconfinfo)
@@ -49,10 +62,19 @@ class GenerateListForBzip2(GenerateList):
         if self._genfull:
             # generate the hash sum of the list of files
             __listhashsum = self._get_list_hash(__listconfinfo['arclistpath'])
+            # define the flexible configuration file path
+            __arcwithext = os.path.split(''.join([__arcpath[:-3], 'conf']))[1]
+            if self.__confoutput:
+                __arcconfpath = os.path.join(self.__confoutput, __arcwithext)
+            elif self.__fulloutput:
+                __arcconfpath = os.path.join(self.__fulloutput, __arcwithext)
+            else:
+                # default
+                __arcconfpath = ''.join([__arcpath[:-3], 'conf'])
             __arcname =  os.path.basename(__arcpath[:-4])
             __confinfo = {'arcname': __arcname,
                             'arcpath': __arcpath,
-                            'arcconfpath': ''.join([__arcpath[:-3],'conf']),
+                            'arcconfpath': __arcconfpath,
                             'arclistpath': __listconfinfo['arclistpath'],
                             'arctype': 'archive',
                             'sha512': __listhashsum}
