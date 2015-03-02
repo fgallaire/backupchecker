1bb2dc408f0a753fb05a55805ce2e599ff52ffa6
Carl Chenet <chaica@ohmytux.com>
2015-03-02 14:04:34 +0100

    modify shebang for tests.py and functionalitests.py

--- functionaltests.py
+++ functionaltests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.4
+#!/usr/bin/env python3.4
 # -*- coding: utf-8 -*-
 # Copyright © 2015 Carl Chenet <chaica@backupcheckerproject.org>
 # This program is free software: you can redistribute it and/or modify
